85094f8097e74388fa707a62aaaf8e08b2e4d0e4
Trside <qwerty_ies@163.com>
2024-05-03 00:57:03 +0800

    Added Renderer2D class.

--- SandBox/assets/shaders/FlatColorShader.glsl
+++ SandBox/assets/shaders/FlatColorShader.glsl
@@ -2,29 +2,23 @@
 #version 460 core
 
 layout(location = 0)in vec2 i_Position;
-layout(location = 1)in vec2 i_TexCoord;
 
 uniform mat4 u_PV;
 uniform mat4 u_Model;
 
-out vec2 v_TexCoord;
-
 void main()
 {
 	gl_Position = u_PV * u_Model * vec4(i_Position, 0.0, 1.0);
-	v_TexCoord = i_TexCoord;
 }
 
 #type fragment
 #version 460 core
 
-in vec2 v_TexCoord;
-
-uniform sampler2D u_Texture;
+uniform vec4 u_FlatColor;
 
 out vec4 color;
 
 void main()
 {
-	color = texture(u_Texture, v_TexCoord);
+	color = u_FlatColor;
 }
